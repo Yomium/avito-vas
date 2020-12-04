e3d1684208d830499a736fc80b331e537750db14
Yomium <yomuim@gmail.com>
2020-12-04 12:38:00 +0300

    Remove image icon caused by image link error

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -12,13 +12,17 @@ class Preview extends React.Component {
     window.open(link);
   }
 
+  handleImageError = (e) => {
+    e.target.style = 'display: none;'
+  }
+
   render() {
     const { banner } = this.props;
     if (Object.keys(banner).length === 0) return null;
     return (
       <div className="preview-wrapper">
       <div id={banner.id} className="story-preview" onClick={this.clickBanner(banner.link)} onKeyPress={this.clickBanner(banner.link)} role="link" tabIndex={0} style={{backgroundColor: banner.color}}>
-        <img src={banner.image} alt={banner.title} />
+        <img src={banner.image} alt="" onError={this.handleImageError}/>
         <div className="story-preview-title">{banner.title ? banner.title.substring(0, 33) : null}</div>
       </div>
       </div>
